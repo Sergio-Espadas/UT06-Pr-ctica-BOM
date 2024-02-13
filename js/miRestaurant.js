98faa741d4c5da592b3b20c3e32eca365c4a44ad
Sergio <sergesp1416@gmail.com>
2024-02-13 19:14:58 +0100

    Añadimos al historyActions las nuevas acciones

--- js/miRestaurant.js
+++ js/miRestaurant.js
@@ -5,6 +5,11 @@ const historyActions = {
     init: () => {
         RestaurantApp.handleInit();
     },
+    CategoryList: (event) => RestaurantApp.handleDishesCategoryList(event.state.category),
+    AllergenList: (event) => RestaurantApp.handleDishesAllergenList(event.state.allergen),
+    MenuList: (event) => RestaurantApp.handleDishesMenuList(event.state.menu),
+    RestaurantList: (event) => RestaurantApp.handleRestaurantList(event.state.restaurant),
+
 };
 
 window.addEventListener('popstate', (event) => {
